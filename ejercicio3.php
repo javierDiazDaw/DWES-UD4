660e64d21f388c9c0775bbcbd7d5c788b79ee0ad
JavierDiazDaw <javier.diaz.roman.al@iespoligonosur.org>
2021-10-25 17:08:47 +0200

    ejercicio 3

--- ejercicio3.php
+++ ejercicio3.php
@@ -4,38 +4,60 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ejercicio 3</title>
 </head>
 <body>
     <?php
+        $file = simplexml_load_file("./catalogo.xml");
+        
+        /*printf("title: %s", $file->book[1]->title);
+        echo "<br>";
+        printf("genre: %s", $file->book[1]->genre);
+        echo "<br>";
+        printf("price: %s", $file->book[1]->price);
+        echo "<br>";
+        printf("author: %s", $file->book[1]->author);
+        echo "<br>";
+        printf("publish_date: %s", $file->book[1]->publish_date);
+        echo "<br>";
+        printf("decription: %s", $file->book[1]->description);
+        echo "<br>";*/
+        /*echo "<table border = 1>";
+        echo "<th>titulo</th> <th>genero</th> <th>precio</th>";
+        foreach ($file as $libro) {
+            //echo '<br>author: '.$libro->author.'<br>';
+            echo "<tr>";
+            echo '<td>'.$libro->title.'</td>';
+            echo '<td>'.$libro->genre.'</td>';
+            echo '<td>'.$libro->price.'</td>';
+            echo "</tr>";
+            //echo 'author: '.$libro->author.'<br>';
+            //echo 'publish_date: '.$libro->publish_date.'<br>';
+            //echo "description: ".$libro->description."<br>\n";
+            
+        }*/
+        
+        $milibro=$file->addChild("book");
+        $milibro->addAttribute("id","bk113");
+        $milibro->addChild("author","Javi");
+        $milibro->addChild("title","Blood");
+        $milibro->addChild("genre","Horror");
+        $milibro->addChild("price","32.2");
+        $milibro->addChild("publish_date","2020-02-02");
+        $milibro->addChild("description","Here we have a horror movie.");
+        $file->asXML("archivo.xml");
 
-    $catalogo = simplexml_load_file("catalogo.xml");
-    $libro2= $file->book[1];
-    printf("<p>Libro 2: %s, </p>", $catalogo->book[1]->author);
-
-
-    echo "<table border = '1'>";
+        echo "<table border = 1>";
+        echo "<th>titulo</th> <th>genero</th> <th>precio</th>";
+        foreach ($file -> book as $lib) {
+            echo "<tr>";
+            echo '<td>'.$lib->title.'</td>';
+            echo '<td>'.$lib->genre.'</td>';
+            echo '<td>'.$lib->price.'</td>';
+            echo "</tr>"; 
+        }
 
         
-
-    foreach($catalogo as $libro){
-
-        echo"<tr>";
-            echo"<td> AUTOR, $libro->author,</td>" ;
-            echo"<td> TITULO, $libro->title,</td>";
-            echo"<td> GENERO, $libro->genre,</td>";
-            echo"<td> PRECIO, $libro->price,</td>";
-            echo"<td> PUBLICACION, $libro->publish_date,</td>";
-            echo"<td> DESCRIPCION, $libro->description,</td>";
-            echo"<br>";
-        echo"</tr>";
-    }
-
-   
-    
-
-
-
     ?>
 </body>
 </html>
