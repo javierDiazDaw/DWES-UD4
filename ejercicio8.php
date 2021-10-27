79c374ea7b64bd5df29be146fcfb036cd426f771
JavierDiazDaw <javier.diaz.roman.al@iespoligonosur.org>
2021-10-27 19:48:25 +0200

    ejercicio 8 incompleto

--- ejercicio8.php
+++ ejercicio8.php
@@ -15,14 +15,13 @@ $servidor = "localhost";
 
     try {
         $conn = new PDO("mysql:host = $servidor; dbname=$baseDatos", $usuario, $pass);
-        echo "Conectado correctamente";
+        //echo "Conectado correctamente";
         echo"<br>";
         $sql = "SELECT * FROM turista";
         $turistas=$conn->query($sql);
-        while($turista = $turistas->fetch()){
-            echo "El turista de nombre ".$turista['nombre']." ".$turista['apellido1']." ".$turista['apellido2']." es de ".$turista['direccion']. "<br />";
-        }
 
+        echo" <p> PDO::FETCH_ASSOC </p>";
+        
         echo" <table border = '1'> ";
             echo" <tr>";
             echo" <th>ID</th>";
@@ -30,6 +29,7 @@ $servidor = "localhost";
             echo" <th>Apellido 1</th>";
             echo" <th>Apellido 2</th>";
             echo" <th>Direccion</th>";
+            echo" <th>Telefono</th>";
             echo "</tr>";
 
             $turistas=$conn->query($sql);
@@ -52,6 +52,9 @@ $servidor = "localhost";
                         echo "</td>";
                         echo "<td>";
                         echo $fila["direccion"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["telefono"];
                         echo "</td>";
                     echo"</tr>";                   
                 }
@@ -65,13 +68,12 @@ $servidor = "localhost";
 
     try {
         $conn = new PDO("mysql:host = $servidor; dbname=$baseDatos", $usuario, $pass);
-        echo "Conectado correctamente";
+        //echo "Conectado correctamente";
         echo"<br>";
         $sql = "SELECT * FROM turista";
         $turistas=$conn->query($sql);
-        while($turista = $turistas->fetch()){
-            echo "El turista de nombre ".$turista['nombre']." ".$turista['apellido1']." ".$turista['apellido2']." es de ".$turista['direccion']. "<br />";
-        }
+
+        echo" <p> PDO::FETCH_NUM </p>";
 
         echo" <table border = '1'> ";
             echo" <tr>";
@@ -80,12 +82,66 @@ $servidor = "localhost";
             echo" <th>Apellido 1</th>";
             echo" <th>Apellido 2</th>";
             echo" <th>Direccion</th>";
+            echo" <th>Telefono</th>";
             echo "</tr>";
 
             $turistas=$conn->query($sql);
                      
                 
-                while($fila = $turistas -> fetch(PDO::FETCH_ASSOC)){
+                while($fila = $turistas -> fetch(PDO::FETCH_NUM)){
+                    
+                    echo "<tr>";
+                        echo "<td>";
+                        echo $fila[0];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila[1];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila[2];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila[3];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila[4];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila[5];
+                        echo "</td>";
+                    echo"</tr>";                   
+                }
+            
+        echo "</table>";
+
+    } catch(PDOException $e){
+        echo "Conexion fallida: " . $e ->getMessage();
+    }
+    $conn = null; //para cerrar conexion
+
+    try {
+        $conn = new PDO("mysql:host = $servidor; dbname=$baseDatos", $usuario, $pass);
+        //echo "Conectado correctamente";
+        echo"<br>";
+        $sql = "SELECT * FROM turista";
+        $turistas=$conn->query($sql);
+
+        echo" <p> PDO::FETCH_BOTH </p>";
+        
+        echo" <table border = '1'> ";
+            echo" <tr>";
+            echo" <th>ID</th>";
+            echo" <th>Nombre</th>";
+            echo" <th>Apellido 1</th>";
+            echo" <th>Apellido 2</th>";
+            echo" <th>Direccion</th>";
+            echo" <th>Telefono</th>";
+            echo "</tr>";
+
+            $turistas=$conn->query($sql);
+                     
+                
+                while($fila = $turistas -> fetch(PDO::FETCH_BOTH)){
                     
                     echo "<tr>";
                         echo "<td>";
@@ -102,6 +158,168 @@ $servidor = "localhost";
                         echo "</td>";
                         echo "<td>";
                         echo $fila["direccion"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["telefono"];
+                        echo "</td>";
+                    echo"</tr>";                   
+                }
+            
+        echo "</table>";
+
+    } catch(PDOException $e){
+        echo "Conexion fallida: " . $e ->getMessage();
+    }
+    $conn = null; //para cerrar conexion
+
+    try {
+        $conn = new PDO("mysql:host = $servidor; dbname=$baseDatos", $usuario, $pass);
+        //echo "Conectado correctamente";
+        echo"<br>";
+        $sql = "SELECT * FROM turista";
+        $turistas=$conn->query($sql);
+
+        echo" <p> PDO::FETCH_OBJ </p>";
+        
+        echo" <table border = '1'> ";
+            echo" <tr>";
+            echo" <th>ID</th>";
+            echo" <th>Nombre</th>";
+            echo" <th>Apellido 1</th>";
+            echo" <th>Apellido 2</th>";
+            echo" <th>Direccion</th>";
+            echo" <th>Telefono</th>";
+            echo "</tr>";
+
+            $turistas=$conn->query($sql);
+                     
+                
+                while($fila = $turistas -> fetch(PDO::FETCH_OBJ)){
+                    
+                    echo "<tr>";
+                    echo "<td>";
+                    printf ("%s", $fila->id);
+                    echo "</td>";
+                    echo "<td>";
+                    printf ("%s", $fila->nombre);
+                    echo "</td>";
+                    echo "<td>";
+                    printf ("%s", $fila->apellido1);
+                    echo "</td>";
+                    echo "<td>";
+                    printf ("%s", $fila->apellido2);
+                    echo "</td>";
+                    echo "<td>";
+                    printf ("%s", $fila->direccion);
+                    echo "</td>";
+                    echo "<td>";
+                    printf ("%s", $fila->telefono);
+                    echo "</td>";  
+                                            
+                }
+            
+        echo "</table>";
+
+    } catch(PDOException $e){
+        echo "Conexion fallida: " . $e ->getMessage();
+    }
+    $conn = null; //para cerrar conexion
+
+    try {
+        $conn = new PDO("mysql:host = $servidor; dbname=$baseDatos", $usuario, $pass);
+        //echo "Conectado correctamente";
+        echo"<br>";
+        $sql = "SELECT * FROM turista";
+        $turistas=$conn->query($sql);
+
+        echo" <p> PDO::FETCH_LAZY </p>";
+        
+        echo" <table border = '1'> ";
+            echo" <tr>";
+            echo" <th>ID</th>";
+            echo" <th>Nombre</th>";
+            echo" <th>Apellido 1</th>";
+            echo" <th>Apellido 2</th>";
+            echo" <th>Direccion</th>";
+            echo" <th>Telefono</th>";
+            echo "</tr>";
+
+            $turistas=$conn->query($sql);
+                     
+                
+                while($fila = $turistas -> fetch(PDO::FETCH_LAZY)){
+                    
+                    echo "<tr>";
+                        echo "<td>";
+                        echo $fila["id"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["nombre"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["apellido1"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["apellido2"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["direccion"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["telefono"];
+                        echo "</td>";
+                    echo"</tr>";                   
+                }
+            
+        echo "</table>";
+
+    } catch(PDOException $e){
+        echo "Conexion fallida: " . $e ->getMessage();
+    }
+    $conn = null; //para cerrar conexion
+
+    try {
+        $conn = new PDO("mysql:host = $servidor; dbname=$baseDatos", $usuario, $pass);
+        //echo "Conectado correctamente";
+        echo"<br>";
+        $sql = "SELECT * FROM turista";
+        $turistas=$conn->query($sql);
+
+    echo" <p> PDO::FETCH_BOUND </p>";
+        
+        echo" <table border = '1'> ";
+            echo" <tr>";
+            echo" <th>ID</th>";
+            echo" <th>Nombre</th>";
+            echo" <th>Apellido 1</th>";
+            echo" <th>Apellido 2</th>";
+            echo" <th>Direccion</th>";
+            echo" <th>Telefono</th>";
+            echo "</tr>";
+
+            $turistas=$conn->query($sql);
+                     
+                
+                while($fila = $turistas -> fetch(PDO::FETCH_BOUND)){
+                    
+                    echo "<tr>";
+                        echo "<td>";
+                        echo $fila-> bincolumn ["id"];
+                        echo "</td>";
+                        echo "<td>"; 
+                        echo $fila["nombre"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["apellido1"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["apellido2"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["direccion"];
+                        echo "</td>";
+                        echo "<td>";
+                        echo $fila["telefono"];
                         echo "</td>";
                     echo"</tr>";                   
                 }
